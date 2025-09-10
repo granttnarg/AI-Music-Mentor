c8732ac08cee70a78453af8bc9f057350a5997d3
granttnarg <48766845+granttnarg@users.noreply.github.com>
2025-09-10 11:43:16 +0200

    test black formatting is working and make change

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-
 import streamlit as st
 
 st.title("AI Music Mentor Dashboard")
-st.markdown("#### Upload your unfinished track to get helpful advice on how to finish it.")
-st.caption("Our AI system is built from data of one experienced producers' feedback and gives advice in their subjective tone and style.")
+st.markdown(
+    "#### Upload your unfinished track to get helpful advice on how to finish it."
+)
+st.caption(
+    "Our AI system is built from data of one experienced producers' feedback and gives advice in their subjective tone and style."
+)
 
 # File upload for MP3
-uploaded_file = st.file_uploader("Upload Unfinished track - MP3 file", type=['mp3'])
+uploaded_file = st.file_uploader("Upload Unfinished track - MP3 file", type=["mp3"])
 
 # Text input
 text_input = st.text_input("What do you need help with on your track?:")
 
 # Dropdown with 3 options
 dropdown_option = st.selectbox(
-    "Stage your track is at:",
-    ["Sketch", "Half Finished", "Almost Finished"]
+    "Stage your track is at:", ["Sketch", "Half Finished", "Almost Finished"]
 )
 
 
